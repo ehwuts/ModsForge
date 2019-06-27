26128b2ffdac5833fc7104cdb98086621683e738
ehwuts matsu <tsukan.git@gmail.com>
2019-06-26 19:48:56 -0700

    a data payload

---
+++ modsforgedata.js
@@ -0,0 +1,78 @@
+var mods = {
+	"mod1": {
+		"name": "A Mod",
+		"description": "This is a mod that does mod stuff.",
+		"versions": [
+			"1.7.10",
+			"1.6.4"
+		],
+		"apis": [
+			"forge"
+		]
+	},
+	"mod2": {
+		"name": "A Different Mod",
+		"description": "This is a totally different mod that does totally different mod stuff.",
+		"versions": [
+			"1.7.10",
+			"1.6.4"
+		],
+		"apis": [
+			"forge"
+		]
+	},
+	"bagelmod": {
+		"name": "Bagel Mod",
+		"description": "This is a mod for all the bagel lovers out there.",
+		"versions": [
+			"1.14.3"
+		],
+		"apis": [
+			"fabric"
+		]
+	},
+	"bagelmodadditions": {
+		"name": "Bagel Mod Additions",
+		"description": "An add-on mod for Bagel Mod, for all the players who love cream cheese.",
+		"versions": [
+			"1.14.3"
+		],
+		"apis": [
+			"fabric"
+		]
+	},
+	"omnimod": {
+		"name": "The Omni Mod",
+		"description": "This is a mod for every version of every thing ever made.",
+		"versions": [
+			"1.14.3",
+			"1.14.2",
+			"1.14.1",
+			"1.14",
+			"1.13.2",
+			"1.13.1",
+			"1.13",
+			"1.12.2",
+			"1.12.1",
+			"1.12",
+			"1.11.2",
+			"1.11",
+			"1.10.2",
+			"1.9.4",
+			"1.9.2",
+			"1.9.1",
+			"1.9",
+			"1.8.9",
+			"1.7.10",
+			"1.7.5",
+			"1.6.4",
+			"1.6.2",
+			"1.6.1",
+			"I got bored of typing these"
+		],
+		"apis": [
+			"fabric",
+			"forge"
+		]
+	}
+};
